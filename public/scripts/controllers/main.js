ded8cde83416f04858b13b556dd736127d8710b0
ArnaudSpanneut <arnauds@kontestapp.com>
2013-03-28 22:32:40 +0100

    Add actions and views for random pledges

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -10,14 +10,13 @@ swallyApp.controller('MainCtrl', function ($scope, Pledges) {
     'pledge': 'A toi de jouer!',
     'description': ''
   };
+  $scope.current_pledges = [];
+  $scope.current_categories = [];
 
   $scope.nb_play = 0;
 
   $scope.getPledge = function () {
-    var pledges = $scope.pledges,
-        pledges_length = pledges.length,
-        pledge = pledges[Math.floor(Math.random() * pledges_length)],
-        nb_play = $scope.nb_play;
+    var nb_play = $scope.nb_play;
 
     if(nb_play !== 0){
       $scope.history_pledges_play.unshift({
@@ -25,7 +24,36 @@ swallyApp.controller('MainCtrl', function ($scope, Pledges) {
         'pledge': $scope.current_pledge
       });
     }
-    $scope.current_pledge = pledge;
+
+    generateRandomPledges(4);
+
     $scope.nb_play += 1;
+  };
+
+  var generateRandomPledges = function(number) {
+    var all_pledges = $scope.pledges,
+        pledges_length = all_pledges.length,
+        categories = [],
+        pledges = [],
+        nb_play = $scope.nb_play;
+
+    // Generate random pledeges.
+    for(var i = 1;  i <= number; i++) {
+      var pledge = all_pledges[Math.floor(Math.random() * pledges_length)],
+          index  = Math.floor(Math.random() * number);
+
+      // Get categories.
+      while(categories[index] != undefined) {
+        index  = Math.floor(Math.random() * number);
+      }
+
+      // Push datas.
+      categories[index] = pledge;
+      pledges.push(pledge);
+    }
+
+    // Set in scope.
+    $scope.current_pledges = pledges;
+    $scope.current_categories = categories;
   };
 });
